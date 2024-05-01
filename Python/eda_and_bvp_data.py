4ea72fcfd6b4710381ecbaf5e9a0525291943af8
Valdemar B. Petersen <valdemarlpt@gmail.com>
2024-05-01 23:06:38 +0200

    Update eda_and_bvp_data.py

--- Python/eda_and_bvp_data.py
+++ Python/eda_and_bvp_data.py
@@ -10,24 +10,42 @@ data, header = pyxdf.load_xdf('test5.xdf')
 
 
 
-# Used for getting markers
-marker_stream = data[0]
-marker_time_stamps = marker_stream["time_stamps"]
-marker_time_series = marker_stream["time_series"]
+def get_marker_time_series(data):
+    marker_stream = data[0]
+    marker_time_stamps = marker_stream["time_stamps"]
+    marker_time_series = marker_stream["time_series"]
+    return marker_time_stamps, marker_time_series
 
-print(marker_time_series)
 
-
-# Used for getting actual data
-data_stream = data[1]
-time_stamps = data_stream["time_stamps"]
-time_series = data_stream["time_series"]
+def get_bvp_and_eda_data(data):
+    data_stream = data[1]
+    data_timestamps = data_stream["time_stamps"]
+    data_timeseries = data_stream["time_series"]
     
-timer = time_stamps
-what = time_series[ :,0]
-bvp = time_series[ :,1]
-eda = time_series[ :,2]
+    huh = data_timeseries[ :,0]
+    bvp = data_timeseries[ :,1]
+    eda = data_timeseries[ :,2]
+    
+    return data_timestamps, huh, bvp, eda
 
-print(eda)
+def analyze_bvp_data(bvp):
+    sampling_rate = 1000
+    processed_data, measures = hp.process(bvp, sampling_rate)
+    
+    print("Heart Rate Measures:")
+    print("BPM:", measures['bpm'])
+    print("IBI:", measures['ibi'])
+    print("SDNN:", measures['sdnn'])
+    print("RMSSD:", measures['rmssd'])
+
+
+
+marker_timestamps, marker_timeseries = get_marker_time_series(data)
+
+data_timestamps, huh, bvp, eda = get_bvp_and_eda_data(data)
+
+# Call the function with the bvp data and sampling rate
+analyze_bvp_data(bvp)
+
 
 
