fb297b09d5b1dd3b34457f3081ce99b058939ee0
Valdemar B. Petersen <valdemarlpt@gmail.com>
2024-05-02 19:46:59 +0200

    .

--- Python/eda_and_bvp_data.py
+++ Python/eda_and_bvp_data.py
@@ -26,11 +26,11 @@ def get_marker_time_series(data):
     for x in range(len(marker_time_stamps)):
         marker_dict[marker_time_stamps[x]] = marker_time_series[x]
         
-    print(marker_dict)
     return marker_time_stamps, marker_time_series
 
 
 def get_bvp_and_eda_data(data):
+    
     data_stream = data[1]
     data_timestamps = data_stream["time_stamps"]
     data_timeseries = data_stream["time_series"]
@@ -41,10 +41,80 @@ def get_bvp_and_eda_data(data):
     
     return data_timestamps, huh, bvp, eda
 
-def analyze_bvp_data(bvp):
+def get_bvp_and_eda_data_for_bugged_data(data):
+    data_stream = data[0]
+    data_timestamps = data_stream["time_stamps"]
+    data_timeseries = data_stream["time_series"]
+    
+    huh = data_timeseries[ :,0]
+    bvp = data_timeseries[ :,1]
+    eda = data_timeseries[ :,2]
+    
+    return data_timestamps, huh, bvp, eda
+
+
+def analyze_bvp_single_data(bvp):
     sampling_rate = 1000
     processed_data, measures = hp.process(bvp, sampling_rate)
     
+    print("Heart Rate Measures:")
+    print("BPM:", measures['bpm'])
+    print("IBI:", measures['ibi'])
+    print("SDNN:", measures['sdnn'])
+    print("RMSSD:", measures['rmssd'])
+    
+def analyze_bvp_baseline_data(bvp_baseline):
+    sampling_rate = 1000
+    processed_data, measures = hp.process(bvp_baseline, sampling_rate)
+    
+    print("\n---------------------------------------------------------------------------------")
+    print("Baseline")
+    print("Heart Rate Measures:")
+    print("BPM:", measures['bpm'])
+    print("IBI:", measures['ibi'])
+    print("SDNN:", measures['sdnn'])
+    print("RMSSD:", measures['rmssd'])
+
+def analyze_bvp_task_data(bvp1, bvp2, bvp3, bvp4, bvp5, sampling_rate=1000):
+    processed_data, measures = hp.process(bvp1, sampling_rate)
+    print("\n---------------------------------------------------------------------------------")
+    print("Task 1 \n")
+    print("Heart Rate Measures:")
+    print("BPM:", measures['bpm'])
+    print("IBI:", measures['ibi'])
+    print("SDNN:", measures['sdnn'])
+    print("RMSSD:", measures['rmssd'])
+    
+    processed_data, measures = hp.process(bvp2, sampling_rate)
+    print("\n---------------------------------------------------------------------------------")
+    print("Task 2 \n")
+    print("Heart Rate Measures:")
+    print("BPM:", measures['bpm'])
+    print("IBI:", measures['ibi'])
+    print("SDNN:", measures['sdnn'])
+    print("RMSSD:", measures['rmssd'])
+    
+    processed_data, measures = hp.process(bvp4, sampling_rate)
+    print("\n---------------------------------------------------------------------------------")
+    print("Task 3 \n")
+    print("Heart Rate Measures:")
+    print("BPM:", measures['bpm'])
+    print("IBI:", measures['ibi'])
+    print("SDNN:", measures['sdnn'])
+    print("RMSSD:", measures['rmssd'])
+    
+    processed_data, measures = hp.process(bvp5, sampling_rate)
+    print("\n---------------------------------------------------------------------------------")
+    print("Task 4 \n")
+    print("Heart Rate Measures:")
+    print("BPM:", measures['bpm'])
+    print("IBI:", measures['ibi'])
+    print("SDNN:", measures['sdnn'])
+    print("RMSSD:", measures['rmssd'])
+    
+    processed_data, measures = hp.process(bvp1, sampling_rate)
+    print("\n---------------------------------------------------------------------------------")
+    print("Task 5 \n")
     print("Heart Rate Measures:")
     print("BPM:", measures['bpm'])
     print("IBI:", measures['ibi'])
@@ -70,17 +140,7 @@ def plot_eda_with_peaks(data_timestamps, eda):
     plt.show()
     
 
-
-marker_timestamps, marker_timeseries = get_marker_time_series(data)
-
-data_timestamps, huh, bvp, eda = get_bvp_and_eda_data(data)
-
-#analyze_bvp_data(bvp)
-#plot_eda_with_peaks(data_timestamps,eda)
-
-
-
-def analyze_eda_data(eda, sampling_rate):
+def analyze_eda_single_data(eda, sampling_rate):
 # SDNN:
 # Normal: 50-100 ms
 # High: >100 ms
@@ -96,20 +156,109 @@ def analyze_eda_data(eda, sampling_rate):
     mean_eda = np.mean(eda)
     return analyze_df, mean_eda
 
-analyze_df, mean_eda = analyze_eda_data(eda, sampling_rate=1000)
-#print(analyze_df)
-#print("Mean EDA:", mean_eda)
+def analyze_eda_baseline_data(eda_baseline, sampling_rate=1000):
+# SDNN:
+# Normal: 50-100 ms
+# High: >100 ms
+# Low: <50 ms
 
-segments = []
-for i, marker in enumerate(marker_timeseries):
-    print(marker[0])
-    if "Started" in marker[0]:
-        start_time = marker_timestamps[i]
-        end_time = marker_timestamps[i + 1]  # Assumes the next marker is the end
-        start_index = np.searchsorted(data_timestamps, start_time)
-        end_index = np.searchsorted(data_timestamps, end_time)
-        segment = bvp[start_index:end_index]
-        segments.append((start_time, end_time, segment))
+# RMSSD:
+# Normal: 20-50 ms (typical for healthy adults)
+# High: >50 ms (common in young, healthy, or well-trained individuals)
+# Low: <20 ms (may indicate stress or health issues)
+
+    print("\n---------------------------------------------------------------------------------")
+    print("Baseline")
+    signals, info = nk.eda_process(eda_baseline, sampling_rate=sampling_rate)
+    analyze_df = nk.eda_analyze(signals, sampling_rate=sampling_rate)
+    mean_eda = np.mean(eda_baseline)
+    print(analyze_df) 
+    print("EDA Mean:  " + str(mean_eda))
+
+def analyze_eda_task_data(eda_task1, eda_task2, eda_task3, eda_task4, eda_task5, sampling_rate):
+    print("\n---------------------------------------------------------------------------------")
+    print("Task 1 \n")
+    signals, info = nk.eda_process(eda_task1, sampling_rate=sampling_rate)
+    analyze_df = nk.eda_analyze(signals, sampling_rate=sampling_rate)
+    mean_eda = np.mean(eda_task1)
+    print(analyze_df) 
+    print("EDA Mean:  " + str(mean_eda))
+    
+    print("---------------------------------------------------------------------------------")
+    print("Task 2 \n")
+    signals, info = nk.eda_process(eda_task2, sampling_rate=sampling_rate)
+    analyze_df = nk.eda_analyze(signals, sampling_rate=sampling_rate)
+    mean_eda = np.mean(eda_task2)
+    print(analyze_df) 
+    print("EDA Mean:  " + str(mean_eda))
+    
+    print("---------------------------------------------------------------------------------")
+    print("Task 3 \n")
+    signals, info = nk.eda_process(eda_task3, sampling_rate=sampling_rate)
+    analyze_df = nk.eda_analyze(signals, sampling_rate=sampling_rate)
+    mean_eda = np.mean(eda_task3)
+    print(analyze_df) 
+    print("EDA Mean:  " + str(mean_eda))
+    
+    print("---------------------------------------------------------------------------------")
+    print("Task 4 \n")
+    signals, info = nk.eda_process(eda_task4, sampling_rate=sampling_rate)
+    analyze_df = nk.eda_analyze(signals, sampling_rate=sampling_rate)
+    mean_eda = np.mean(eda_task4)
+    print(analyze_df) 
+    print("EDA Mean:  " + str(mean_eda))
+    
+    print("---------------------------------------------------------------------------------")
+    print("Task 5 \n")
+    signals, info = nk.eda_process(eda_task5, sampling_rate=sampling_rate)
+    analyze_df = nk.eda_analyze(signals, sampling_rate=sampling_rate)
+    mean_eda = np.mean(eda_task5)
+    print(analyze_df) 
+    print("EDA Mean:  " + str(mean_eda))
+    
+
+def extract_task_segments(marker_timestamps, marker_timeseries, data_timestamps, dataz):
+    segments = []
+    for i, marker in enumerate(marker_timeseries):
+        if "Started" in marker[0]:
+            start_time = marker_timestamps[i]
+            end_time = marker_timestamps[i + 1]  # Assumes the next marker is the end
+            start_index = np.searchsorted(data_timestamps, start_time)
+            end_index = np.searchsorted(data_timestamps, end_time)
+            segment = dataz[start_index:end_index]
+            segments.append((start_time, end_time, segment))
         
-print(segments)
-print(len(segments))
+    task1_data = segments[0][2]
+    task2_data = segments[1][2]
+    task3_data = segments[2][2]
+    task4_data = segments[3][2] 
+    task5_data = segments[4][2]
+    
+    return task1_data, task2_data, task3_data, task4_data, task5_data
+    
+
+
+def main():
+    
+
+    marker_timestamps, marker_timeseries = get_marker_time_series(data)
+
+    data_timestamps, huh, bvp, eda = get_bvp_and_eda_data(data)
+    #data_timestamps, huh, bvp, eda = get_bvp_and_eda_data_for_bugged_data(data)
+
+    # BVP
+    #analyze_bvp_data(bvp)
+
+
+    analyze_bvp_baseline_data(bvp)
+    analyze_eda_baseline_data(eda)
+
+    #task1_bvp, task2_bvp, task3_bvp, task4_bvp, task5_bvp = extract_task_segments(marker_timestamps, marker_timeseries, data_timestamps, bvp)
+    #task1_eda, task2_eda, task3_eda, task4_eda, task5_eda = extract_task_segments(marker_timestamps, marker_timeseries, data_timestamps, eda)
+    
+    #analyze_eda_task_data(task1_eda, task2_eda, task3_eda, task4_eda, task5_eda, sampling_rate=1000)
+    #analyze_bvp_task_data(task1_bvp, task2_bvp, task3_bvp, task4_bvp, task5_bvp, sampling_rate=1000)
+
+
+if __name__ == "__main__":
+    main()
