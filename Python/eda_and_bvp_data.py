31b8f09ed529f7cbe488b9c2260ef2795f1e7c11
Valdemar B. Petersen <valdemarlpt@gmail.com>
2024-05-02 13:57:00 +0200

    28

--- Python/eda_and_bvp_data.py
+++ Python/eda_and_bvp_data.py
@@ -1,5 +1,4 @@
 import pyxdf
-import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime, timedelta
 import time
@@ -7,14 +6,21 @@ import heartpy as hp
 from scipy.signal import find_peaks
 import neurokit2 as nk
 
-data, header = pyxdf.load_xdf('test6.xdf')
+import matplotlib.pyplot as plt
 
+data, header = pyxdf.load_xdf('test6.xdf')
 
 
 def get_marker_time_series(data):
     marker_stream = data[0]
     marker_time_stamps = marker_stream["time_stamps"]
     marker_time_series = marker_stream["time_series"]
+
+    marker_dict = {}
+    for x in range(len(marker_time_stamps)):
+        marker_dict[marker_time_stamps[x]] = marker_time_series[x]
+        
+    print(marker_dict)
     return marker_time_stamps, marker_time_series
 
 
@@ -57,19 +63,37 @@ def plot_eda_with_peaks(data_timestamps, eda):
     plt.legend()
     plt.show()
     
-    
 
 
 marker_timestamps, marker_timeseries = get_marker_time_series(data)
 
 data_timestamps, huh, bvp, eda = get_bvp_and_eda_data(data)
 
-# Call the function with the bvp data and sampling rate
 analyze_bvp_data(bvp)
 #plot_eda_with_peaks(data_timestamps,eda)
 
 
 
-signals, info = nk.eda_process(eda, sampling_rate=1000)
-analyze_df = nk.eda_analyze(signals, sampling_rate=1000)
+def analyze_eda_data(eda, sampling_rate):
+# SDNN:
+# Normal: 50-100 ms
+# High: >100 ms
+# Low: <50 ms
+
+# RMSSD:
+# Normal: 20-50 ms (typical for healthy adults)
+# High: >50 ms (common in young, healthy, or well-trained individuals)
+# Low: <20 ms (may indicate stress or health issues)
+
+    signals, info = nk.eda_process(eda, sampling_rate=sampling_rate)
+    analyze_df = nk.eda_analyze(signals, sampling_rate=sampling_rate)
+    mean_eda = np.mean(eda)
+    return analyze_df, mean_eda
+
+analyze_df, mean_eda = analyze_eda_data(eda, sampling_rate=1000)
 print(analyze_df)
+print("Mean EDA:", mean_eda)
+
+eda_signals, info = nk.eda_process(eda, sampling_rate=1000)
+
+nk.eda_plot(eda_signals, info)
